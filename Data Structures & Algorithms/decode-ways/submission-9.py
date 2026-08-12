ab42216b0dfa48277847b3f620c4fe4cd27290bf
Thomas Tse <59314357+thomasyunghint@users.noreply.github.com>
2026-08-12 10:58:44 -0700

    Add: decode-ways - submission-9

---
+++ Data Structures & Algorithms/decode-ways/submission-9.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        #memoiztation cuz redo sub problem
+
+        dp = {len(s):1}
+        def dfs(i):
+            #if in already, use it
+            if i in dp:
+                return dp[i]
+            #if s[i] is 0, return 0 
+            if s[i] == "0":
+                return 0
+            #1 digit case
+            res = dfs(i+1)
+            #2 digit case
+            if (i+1 <len(s) and (s[i]=="1" or (s[i]== "2" and s[i+1] in "0123456"))):
+                res += dfs(i+2)
+            #save in hashmaap
+            dp[i] = res
+            return res
+        #return largest problem
+        return dfs(0)
