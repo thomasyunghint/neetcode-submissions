948025f57ac79e6ec670beddd46a63f03afbe81b
Thomas Tse <59314357+thomasyunghint@users.noreply.github.com>
2026-08-17 18:55:27 -0700

    Add: rotting-fruit - submission-20

---
+++ Data Structures & Algorithms/rotting-fruit/submission-20.py
@@ -0,0 +1,26 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        fresh, time =0, 0
+        q = deque()
+        visit = set()
+        #bfs since diff rotten spread at the same time
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh+=1
+                if grid[r][c] == 2:
+                    q.append((r,c))
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
+        while q and fresh > 0:
+            for _ in range(len(q)):
+                r,c = q.popleft()
+                for dr, dc in directions:
+                    row = r+dr
+                    col  = c+dc
+                    if (row in range(len(grid))) and col in range(len(grid[0])) and grid[row][col] == 1:
+                        grid[row][col] = 2
+                        q.append((row,col))
+                        fresh -=1
+            time +=1
+        return time if fresh == 0 else -1
