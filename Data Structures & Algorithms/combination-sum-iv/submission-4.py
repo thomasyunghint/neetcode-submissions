ae0221739963859f208dd8183bc621722341e339
Thomas Tse <59314357+thomasyunghint@users.noreply.github.com>
2026-08-30 13:51:06 -0700

    Add: combination-sum-iv - submission-4

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        #dp[i]: number of possible combinations from i to n
+        dp = [0]*(target+1)
+        dp[target] = 1
+        for i in range(target-1,-1,-1):
+            for c in nums:
+                if i + c <= target:
+                    dp[i] += dp[i+c]
+        return dp[0]
